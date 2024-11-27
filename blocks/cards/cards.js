9f5a20b1a775fa849db13b69efe2193cd76b6dda
KP Parrish <krparrish@adobe.com>
2024-11-27 01:42:59 -0700

    update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,18 +2,37 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
+	const isIconCards = block.classList.contains('icon');
+	const isArticleCards = block.classList.contains('articles');
+
   /* change to ul, li */
   const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    moveInstrumentation(row, li);
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
-  });
+
+  if (isIconCards) {
+	[...block.children].forEach((row) => {
+		const anchor = document.createElement('a');
+		anchor.href = '';
+		const li = document.createElement('li');
+		moveInstrumentation(row, li);
+		while (row.firstElementChild) li.append(row.firstElementChild);
+		[...li.children].forEach((div) => {
+			if (div.children.length === 1 && div.querySelector('a')) {
+				const linkURL = div.querySelector('a').innerHTML;
+				anchor.href = linkURL;
+				div.className = 'cards-hide-markdown';
+			  } else if (div.children.length === 1 && div.querySelector('picture')) {
+				div.className = 'cards-card-image';
+			  } else if (div.children.length === 1 && div.querySelector('span')) {
+				div.className = 'cards-card-icon';
+			  } else {
+				div.className = 'cards-card-body';
+			  }
+			});
+		anchor.append(li);
+		ul.append(anchor);
+	});
+  }
+
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     moveInstrumentation(img, optimizedPic.querySelector('img'));
