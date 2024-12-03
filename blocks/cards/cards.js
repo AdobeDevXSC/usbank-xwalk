ff71f112d5c7ccafba3fc66050aeaaa1b1751aa2
KP Parrish <krparrish@adobe.com>
2024-12-03 01:32:54 -0700

    updated styles for article hero and article block

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -5,16 +5,51 @@ export default async function decorate(block) {
 	const isIconCards = block.classList.contains('is-icon');
 	const isArticleCards = block.classList.contains('is-json');
 
-	async function fetchJson(link) {
+	if (isArticleCards) {
+		console.log(isArticleCards);
+
+		const link = block.querySelector('p > a');
 		const response = await fetch(link?.href);
-	
-		if (response.ok) {
-		  const jsonData = await response.json();
-		  const data = jsonData?.data;
-		  return data;
+
+		if (!response.ok) {
+			return 'an error occurred';
 		}
-		return 'an error occurred';
-	}
+
+
+		console.log(link, response.json());
+		// const cardData = jsonData.data;
+		// const cards = []
+
+
+	  
+	//   cardData.forEach((item, index) => {
+	// 	  const picture = createOptimizedPicture(item.image, item.title, false, [{ width: 320 }]);
+	// 	  picture.lastElementChild.width = '320';
+	// 	  picture.lastElementChild.height = '180';
+
+	// 	  const createdCard = document.createElement('li');
+	// 	//   moveInstrumentation(item, createdCard);
+
+	// 	  createdCard.innerHTML = `
+	// 		<div class="cards-card-image">
+	// 		  <div data-align="center">${picture.outerHTML}</div>
+	// 		</div>
+	// 		<div class="cards-card-body">
+	// 		  <h5>${item.title}</h5>
+	// 		  <p class="button-container">
+	// 			<a href="${item.url}" aria-label="${item['anchor-text']}" title="${item['anchor-text']}" class="button">
+	// 			  Read more 
+	// 			  <span class="card-arrow">
+	// 				<img class="icon" src="/icons/chevron.svg" />
+	// 			  </span>
+	// 			</a>
+	// 		  </p>
+	// 		</div>
+	// 	  `;
+	// 	  ul.append(createdCard);
+	// 	})
+
+}
 	
   /* change to ul, li */
   const ul = document.createElement('ul');
@@ -23,12 +58,17 @@ export default async function decorate(block) {
 	[...block.children].forEach((row) => {
 		const anchor = document.createElement('a');
 		anchor.href = '';
+		
 		const li = document.createElement('li');
 		moveInstrumentation(row, li);
+
 		while (row.firstElementChild) li.append(row.firstElementChild);
 		[...li.children].forEach((div) => {
 			if (div.children.length === 1 && div.querySelector('a')) {
 				const linkURL = div.querySelector('a').innerHTML;
+				if(div.querySelector('a')){
+					console.log(div)
+				}
 				anchor.href = linkURL;
 				div.className = 'cards-hide-markdown';
 			  } else if (div.children.length === 1 && div.querySelector('picture')) {
@@ -40,35 +80,6 @@ export default async function decorate(block) {
 		anchor.append(li);
 		ul.append(anchor);
 	})
-  } else if (isArticleCards) {
-	const link = block.querySelector('p > a');
-	const cardData = fetchJson(link);
-	console.log(link, cardData);
-  
-  cardData.forEach((item) => {
-      const picture = createOptimizedPicture(item.image, item.title, false, [{ width: 320 }]);
-      picture.lastElementChild.width = '320';
-      picture.lastElementChild.height = '180';
-      const createdCard = document.createElement('li');
-	  moveInstrumentation(item, createdCard);
-      createdCard.innerHTML = `
-        <div class="cards-card-image">
-          <div data-align="center">${picture.outerHTML}</div>
-        </div>
-        <div class="cards-card-body">
-          <h5>${item.title}</h5>
-          <p class="button-container">
-            <a href="${item.url}" aria-label="${item['anchor-text']}" title="${item['anchor-text']}" class="button">
-              Read more 
-              <span class="card-arrow">
-                <img class="icon" src="/icons/chevron.svg" />
-              </span>
-            </a>
-          </p>
-        </div>
-      `;
-      ul.append(createdCard);
-    })
   } else {
 	[...block.children].forEach((row) => {
 		const li = document.createElement('li');
