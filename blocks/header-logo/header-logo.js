99e45630440bf4f9a5209f3cd5e39dd6b5697b39
KP Parrish <krparrish@adobe.com>
2024-12-03 21:33:53 -0700

    removed console.log from header logo

--- blocks/header-logo/header-logo.js
+++ blocks/header-logo/header-logo.js
@@ -2,7 +2,6 @@ export default async function decorate(block) {
 	  const logoPic = block.querySelector('div > picture');
 	  const anchorTag = block.querySelector('a');
 
-	  console.log(anchorTag, logoPic)
 	  block.innerHTML = `
 	  	<a class="" href=${anchorTag.href} title="home">
 			${logoPic.outerHTML}
