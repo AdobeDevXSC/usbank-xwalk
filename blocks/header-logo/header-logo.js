70c33143fdd2089205b16d614a0d6d69c2731662
KP Parrish <krparrish@adobe.com>
2024-12-03 21:15:55 -0700

    fixed header and header-logo styles for mobile and desktop

--- blocks/header-logo/header-logo.js
+++ blocks/header-logo/header-logo.js
@@ -1,19 +1,12 @@
 export default async function decorate(block) {
-	[...block.children].forEach((child) => {
-	  const iconName = child.querySelector('img').getAttribute('data-icon-name');
-	  child.querySelector('img').alt = iconName;
-	  // add for xwalk
-	  const spanTag = child.querySelector('span');
-	  const anchorTag = child.querySelector('a');
-	  if (anchorTag && !anchorTag.querySelector('span')) {
-		// in x-walk, move the span tag inside the anchor a tag and delete the outside span tag
-		if (spanTag && spanTag.parentElement.tagName.toLowerCase() === 'p') {
-		  const parentPTag = spanTag.parentNode;
-		  parentPTag.parentNode.removeChild(parentPTag);
-		}
+	  const logoPic = block.querySelector('div > picture');
+	  const anchorTag = block.querySelector('a');
+
+	  console.log(anchorTag, logoPic)
+	  block.innerHTML = `
+	  	<a class="" href=${anchorTag.href} title="home">
+			${logoPic.outerHTML}
+		</a>`;
+
 		anchorTag.textContent = '';
-		anchorTag.appendChild(spanTag);
-		anchorTag.title = iconName;
-	  }
-	});
   }
